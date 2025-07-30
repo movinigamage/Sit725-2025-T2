a0be651b508cfad81a794b831ca6dec760098c6f
Movini Gamage <59785057+movinigamage@users.noreply.github.com>
2025-07-30 22:16:48 +1000

    Final commit for SIT725 Task 3.2P - Added contact form script and final UI polish

--- wk03/sit725-2021-t2-prac3/server.js
+++ wk03/sit725-2021-t2-prac3/server.js
@@ -1,12 +1,25 @@
- var express= require("express")
- var app= express()
+//  var express= require("express")
+//  var app= express()
 
- app.use(express.static(__dirname+'/public'))
- app.use(express.json());
- app.use(express.urlencoded({ extended:false}));
+//  app.use(express.static(__dirname+'/public'))
+//  app.use(express.json());
+//  app.use(express.urlencoded({ extended:false}));
 
- var port= process.env.port|| 3000;
+//  var port= process.env.port|| 3000;
 
- app.listen(port,()=>{
- console.log("App listening to: "+port)
- })
+//  app.listen(port,()=>{
+//  console.log("App listening to: "+port)
+//  })
+
+
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log("App listening to: " + port)
+})
